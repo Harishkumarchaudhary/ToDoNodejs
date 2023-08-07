582d7765ea63bc2c486cf3ed50e0e5e705100b04
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-08-07 19:34:17 +0530

    return response

--- contactlist/index.js
+++ contactlist/index.js
@@ -4,6 +4,10 @@ const port = 8000;
 
 const app = express();
 
+app.get('/', function(req, res){
+    res.send('<h1>Cool it is running</h1>')
+})
+
 app.listen(port, function(err){
     if (err) {
         console.log('Error', err);
